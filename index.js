1fd39078ae6e38d55a993c9327b4e87d5b562c78
gwenael3.bihan@epitech.eu <gwenael3.bihan@epitech.eu>
2022-04-22 15:23:51 +0200

    yo

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+var udp = require('dgram');
+var client = udp.createSocket('udp4');
+var server = udp.createSocket("udp4");
+
+server.on("message", function (msg) {
+    process.stdout.write("UDP String: " + msg + "\n");
+    process.exit();
+})
+    .bind(2205, () => {
+        server.setSendBufferSize(12345);
+        const size = server.getSendBufferSize();
+        console.log(size);
+
+    });
+
+// Client sending message to server
+client.send("Hello", 0, 7, 1234, "localhost");
+app = express(),
+    port = process.env.PORT || 3000;
+
+app.listen(port);
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/trash', (req, res) => res.send('data'));
+
+console.log('book list RESTful API server started on: ' + port);
